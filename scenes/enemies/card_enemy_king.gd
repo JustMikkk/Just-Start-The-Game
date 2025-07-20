80fbb9bac72d12ba5477f290b831f92856ba5594
JustMikkk <golaszevskymikolaj@gmail.com>
2025-07-20 21:44:43 +0200

    fix: death of the king

--- scenes/enemies/card_enemy_king.gd
+++ scenes/enemies/card_enemy_king.gd
@@ -4,6 +4,18 @@ const CARD_ENEMY = preload("res://scenes/enemies/card_enemy.tscn")
 
 @onready var _animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
+func _ready() -> void:
+	damage_taken.connect(func():
+		_animated_sprite_2d.modulate = Color.RED
+		await get_tree().create_timer(0.1).timeout
+		_animated_sprite_2d.modulate = Color.WHITE
+	)
+	entity_died.connect(func():
+		queue_free()
+	)
+
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		_animated_sprite_2d.speed_scale = 0
